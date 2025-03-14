b730bf6eea72d5ce075e8250e1653fc478e37508
ankathi_sathwika <ankathi_sathwika@epam.com>
2025-03-14 11:03:16 +0530

    updated

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -2,13 +2,12 @@ package com.task03;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 @LambdaHandler(
 		lambdaName = "hello_world",
@@ -19,29 +18,20 @@ import java.util.Map;
 )
 public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
-	private final ObjectMapper objectMapper = new ObjectMapper(); // JSON Serializer
+	private static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
 
 	@Override
-	public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
-		context.getLogger().log("Hello from Lambda");
+	public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+		LOGGER.info("Received event: " + event);
 
-		// Create response map
+		// TODO: Implement business logic
+		return createResponse();
+	}
+
+	private Map<String, Object> createResponse() {
 		Map<String, Object> response = new HashMap<>();
 		response.put("statusCode", 200);
-		response.put("headers", Map.of("Content-Type", "application/json"));
-
-		// JSON body
-		Map<String, String> responseBody = new HashMap<>();
-		responseBody.put("message", "Hello from Lambda");
-
-		try {
-			response.put("body", objectMapper.writeValueAsString(responseBody));
-		} catch (JsonProcessingException e) {
-			context.getLogger().log("Error serializing response: " + e.getMessage());
-			response.put("statusCode", 500);
-			response.put("body", "{\"message\": \"Internal server error\"}");
-		}
-
+		response.put("message", "Hello from Lambda");
 		return response;
 	}
 }
